f61640f62045935fbc49071963b9231a07170098
ChrisAble2Shine <148521238+ChrisAble2Shine@users.noreply.github.com>
2025-12-14 16:30:43 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,57 @@
+function createAsteroids () {
+    info.startCountdown(15)
+    asteroidList = [
+    assets.image`asteroid0`,
+    assets.image`asteroid1`,
+    assets.image`asteroid2`,
+    assets.image`spaceDebris0`,
+    assets.image`spaceDebris1`
+    ]
+    while (info.countdown() > 0) {
+        projectile = sprites.createProjectileFromSide(asteroidList._pickRandom(), randint(-75, -25), randint(-25, 25))
+        projectile.setPosition(160, randint(5, 115))
+        pause(randint(250, 1000))
+    }
+}
+function startGame () {
+    info.setLife(3)
+    scene.setBackgroundImage(assets.image`spaceBackground`)
+    scroller.scrollBackgroundWithSpeed(-50, 0)
+    discovery = sprites.create(assets.image`discoveryShuttle`, SpriteKind.Player)
+    discovery.setPosition(30, 60)
+    discovery.z = 10
+    controller.moveSprite(discovery, 75, 75)
+    discovery.setStayInScreen(true)
+    createAsteroids()
+}
+info.onCountdownEnd(function () {
+    hubble = sprites.create(assets.image`hubbleTelescope`, SpriteKind.Player)
+    hubble.setPosition(140, 55)
+    music.play(music.melodyPlayable(music.beamUp), music.PlaybackMode.InBackground)
+    scroller.scrollBackgroundWithSpeed(0, 0)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite)
+    info.changeLifeBy(-1)
+    music.play(music.melodyPlayable(music.bigCrash), music.PlaybackMode.InBackground)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    sprites.destroyAllSpritesOfKind(SpriteKind.Projectile)
+    music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.InBackground)
+    scene.setBackgroundImage(assets.image`kellyScreen`)
+    game.showLongText("You have successfully navigated the Discovery to the Hubble", DialogLayout.Bottom)
+    game.showLongText("Thank you for helping me", DialogLayout.Bottom)
+    game.reset()
+})
+let hubble: Sprite = null
+let discovery: Sprite = null
+let projectile: Sprite = null
+let asteroidList: Image[] = []
+scene.setBackgroundImage(assets.image`splashScreen`)
+game.showLongText("A trip to the Hubble", DialogLayout.Bottom)
+scene.setBackgroundImage(assets.image`kellyScreen`)
+game.showLongText("I am astronaut Scott Kelly", DialogLayout.Bottom)
+game.showLongText("And I need your help to navigate the Discovery", DialogLayout.Bottom)
+game.showLongText("Use the arrow keys to move the Discovery", DialogLayout.Bottom)
+startGame()
